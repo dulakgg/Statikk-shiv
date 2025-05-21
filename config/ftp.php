98cf056480a949385211680a751991a8e387b3b0
dulak <140708360+Kubak2673@users.noreply.github.com>
2025-05-21 07:49:00 +0200

    security changes build automations.

--- config/ftp.php
+++ config/ftp.php
@@ -4,4 +4,6 @@ return [
     'host' => env('FTP_HOST'),
     'username' => env('FTP_USERNAME'),
     'password' => env('FTP_PASSWORD'),
+    'deploy_url' => env('FTP_DEPLOY_URL'),
+    'deploy_token' => env('FTP_DEPLOY_TOKEN'),
 ];
